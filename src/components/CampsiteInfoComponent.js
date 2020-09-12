3b4d4b93aba496c9c4d127cbfa64cb746d945861
Walter Neto <wneto.macedo@gmail.com>
2020-09-12 11:40:18 -0500

    Workshop Assigment #2 constructor fix

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-
-    }
 
     renderCampsite(campsite) {
         return (
