d6944fa659f6002adff8f97abe360623de695824
Walter Neto <wneto.macedo@gmail.com>
2020-09-19 11:45:11 -0500

    Moved RenderPartner Function

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,6 +3,18 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 
 
+function RenderPartner({partner}) {
+    if (partner) {
+        return (
+            <React.Fragment>
+                <Media object src={partner.image} alt={partner.name} width="150" />
+                <Media body className="ml-5 mb-4"><Media heading>{partner.name}</Media>{partner.description}</Media>
+            </React.Fragment>
+        );
+    }
+    return <div />
+}
+
 function About(props) {
 
     const partners = props.partners.map(partner => {
@@ -10,18 +22,6 @@ function About(props) {
             <Media tag="li" key={partner.id}><RenderPartner partner={partner} /></Media>
         );
     });
-
-    function RenderPartner({partner}) {
-        if (partner) {
-            return (
-                <React.Fragment>
-                    <Media object src={partner.image} alt={partner.name} width="150" />
-                    <Media body className="ml-5 mb-4"><Media heading>{partner.name}</Media>{partner.description}</Media>
-                </React.Fragment>
-            );
-        }
-        return <div />
-    }
 
     return (
         <div className="container">
